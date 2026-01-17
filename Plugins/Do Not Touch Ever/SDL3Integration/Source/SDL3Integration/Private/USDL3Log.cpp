f8dea7a724d7aacb83d5fda61aee25d9d8fa0690
matthew pietrafesa <mattpietrafesa@gmail.com>
2026-01-17 17:31:03 -0500

    Delete all files in the repository

--- Plugins/Do Not Touch Ever/SDL3Integration/Source/SDL3Integration/Private/USDL3Log.cpp
+++
@@ -1,8 +0,0 @@
-#include "USDL3Log.h"
-#include "SDL3Integration.h"
-
-
-// Define the log category
-DEFINE_LOG_CATEGORY(LogSDL3);
-
-
--- Plugins/Do Not Touch Ever/SDL3Integration/Source/SDL3Integration/Public/USDL3Log.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "CoreMinimal.h"
-
-// Declare custom log category
-DECLARE_LOG_CATEGORY_EXTERN(LogSDL3, Log, All);
